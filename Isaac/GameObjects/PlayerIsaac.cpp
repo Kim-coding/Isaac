6dede064b2fba37cedeb658409e702d8572af5a2
kim_Bo <qhgus5459@naver.com>
2024-03-06 11:15:00 +0900

    Player Isaac 수정

--- Isaac/GameObjects/PlayerIsaac.cpp
+++ Isaac/GameObjects/PlayerIsaac.cpp
@@ -1,6 +1,13 @@
 #include "pch.h"
 #include "PlayerIsaac.h"
 
+std::string PlayerIsaac::IdleDown = "animators/IdleDown.csv";
+std::string PlayerIsaac::IdleSide = "animators/IdleSide.csv";
+std::string PlayerIsaac::IdleUp = "animators/IdleUP.csv";
+std::string PlayerIsaac::MoveDown = "animators/MoveDown.csv";
+std::string PlayerIsaac::MoveSide = "animators/MoveSide.csv";
+std::string PlayerIsaac::MoveUp = "animators/MoveUp.csv";
+
 PlayerIsaac::PlayerIsaac(const std::string& name)
 	:SpriteGo(name)
 {
@@ -10,26 +17,87 @@ PlayerIsaac::~PlayerIsaac()
 {
 }
 
+void PlayerIsaac::TestInstance()
+{
+}
+
+void PlayerIsaac::TestStaic()
+{
+}
+
 void PlayerIsaac::Init()
 {
 	SpriteGo::Init();
+
+	animator.SetTarget(&sprite);
+
+	clipInfos.push_back({ IdleSide, MoveSide, true, Utils::GetNormal({-1,-1}) });
+	clipInfos.push_back({ IdleUp, MoveUp, false, {0, -1} });
+	clipInfos.push_back({ IdleSide, MoveSide, false, Utils::GetNormal({1,-1}) });
+
+	clipInfos.push_back({ IdleSide, MoveSide, true, {-1, 0} });
+	clipInfos.push_back({ IdleSide, MoveSide, false, {1, 0} });
+
+	clipInfos.push_back({ IdleSide, MoveSide, true, Utils::GetNormal({-1,1}) });
+	clipInfos.push_back({ IdleDown, MoveDown, false, {0, 1} });
+	clipInfos.push_back({ IdleSide, MoveSide, false, Utils::GetNormal({1, 1}) });
+
+
 }
 
-void PlayerIsaac::Release()
+void PlayerIsaac::Reset()
 {
-	SpriteGo::Release();
+	animator.Play(IdleDown);
+	SetOrigin(Origins::BC);
+	SetPosition({ 0.f,0.f });
+	SetFlipX(false);
+
+	currentClipInfo = clipInfos[6];
 }
 
 void PlayerIsaac::Update(float dt)
 {
-	SpriteGo::Update(dt);
+	animator.Update(dt);
+
+	direction.x = InputMgr::GetAxisRaw(Axis::Horizontal);
+	direction.y = InputMgr::GetAxisRaw(Axis::Vertical);
+
+	float mag = Utils::Magnitude(direction);
+	if (mag > 1.f)            //대각 이동 속도가 빨라지게 되므로
+	{
+		direction /= mag;
+	}
+	Translate(direction * speed * dt);
+
+	if (direction.x != 0.f || direction.y != 0.f)
+	{
+		auto min = std::min_element(clipInfos.begin(), clipInfos.end(),
+			[this](const ClipInfo& lhs, const ClipInfo& rhs)
+			{
+				return Utils::Distance(direction, lhs.point) < Utils::Distance(direction, rhs.point);
+			});
+		currentClipInfo = *min;
+	}
+
+	SetFlipX(currentClipInfo.filpX);
+
+	const std::string& clipId = (direction.x != 0.f || direction.y != 0.f) ?
+		currentClipInfo.move : currentClipInfo.idle;
+
+	if (animator.GetCurrentCilpId() != clipId)
+	{
+		animator.Play(clipId);
+	}
 }
 
-void PlayerIsaac::Draw(sf::RenderWindow& window)
+void PlayerIsaac::Cry() //공격
 {
-	SpriteGo::Draw(window);
 }
 
-void PlayerIsaac::Cry()
+void PlayerIsaac::OnDamage(int damage)
+{
+}
+
+void PlayerIsaac::OnDie()
 {
 }
